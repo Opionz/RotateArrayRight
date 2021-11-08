041aff1f92bc493896234242b0bd2c510f9312a5
Opionz <ope.ajayi@hotmail.com>
2021-11-08 07:57:49 +0100

    Add project files.

---
+++ RotateArrayRight/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace RotateArrayRight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //Rotate array to the right of a given pivot
+        public static int[] Rotate(int[] x, int pivot)
+        {
+            if (pivot <= 0 || x == null)
+                throw new Exception("Invalid argument");
+
+            pivot = pivot % x.Length;
+
+            //Rotate first half
+            x = RotateSub(x, 0, pivot - 1);
+
+            //Rotate second half
+            x = RotateSub(x, pivot, x.Length - 1);
+
+            //Rotate all
+            x = RotateSub(x, 0, x.Length - 1);
+
+            return x;
+        }
+
+        private static int[] RotateSub(int[] x, int start, int end)
+        {
+            while (start <= end)
+            {
+                int temp = x[start];
+                x[start] = x[end];
+                x[end] = temp;
+                start++;
+                end--;
+            }
+            return x;
+        }
+    }
+}
